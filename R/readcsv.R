3be5997a383f6c96f27b7eb9a56fdbb655325d79
sreejithag <sreejithag97@gmail.com>
2019-04-30 10:45:17 +0530

    deletions

--- R/readcsv.R
+++
@@ -1,9 +0,0 @@
-
-#load RNeorj libraray
-library(RNeo4j)
-
-
-#connect to neo4j server
-graph = startGraph("http://54.237.227.129:34703/db/data/", username = "neo4j", password = "arm-deserters-services")
-
-
