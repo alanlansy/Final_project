5051f1349f1b0bb1f2829b0f584cf6b3932e38b1
sreejithag <sreejithag97@gmail.com>
2019-05-07 20:55:08 +0530

    bug fix

--- prediction/Flask/app.py
+++ prediction/Flask/app.py
@@ -15,19 +15,19 @@ app2 = Flask(__name__)
 
 ######## Preparing the Classifier
 cur_dir = os.path.dirname(__file__)
-clf = joblib.load('xgb_model.pkl')
+clf = joblib.load('model.pkl')
 booster = clf.get_booster()
 booster.set_param({'predictor': 'cpu_predictor'})
 col_names=joblib.load('col_names.pkl')
 
 def classify(document):
-    pkl_file = open('crime_categories.pkl', 'rb')
-    crime = pickle.load(pkl_file)
-    feat_array=np.zeros(len(col_names))
-    feat_array[-3]=document[0]
-    feat_array[-2]=document[1]
-    feat_array[-1]=document[2]
-    h=feat_array.reshape(len(feat_array), 1)
+    crime_array = open('crime_categories.pkl', 'rb')
+    crime = pickle.load(crime_array)
+    temp_array=np.zeros(len(col_names))
+    temp_array[-3]=document[0]
+    temp_array[-2]=document[1]
+    temp_array[-1]=document[2]
+    h=temp_array.reshape(len(temp_array), 1)
     X=pd.DataFrame(h.T, columns=col_names)
     y = clf.predict(X)
     result=clf.predict_proba(X)
@@ -35,10 +35,6 @@ def classify(document):
     return crime[y[0]], proba
 
 
-month_list=['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep',
-   'Oct', 'Nov', 'Dec']
-neighborhood_list=['BAYVIEW', 'CENTRAL', 'INGLESIDE', 'MISSION',
-   'NORTHERN', 'PARK', 'RICHMOND', 'SOUTHERN', 'TARAVAL', 'TENDERLOIN']
 hour_list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
 ######## Flask
@@ -59,11 +55,6 @@ def results():
     data=[longitude, latitude, int(hour)]
     y, proba = classify(data)
     return jsonify({'prob':round(proba*100, 2),'cat':y})
-
-        # return render_template('results.html',
-        #                         data=data,
-        #                         prediction=y,
-        #                         probability=round(proba*100, 2))
 
 
 if __name__ == '__main__':
